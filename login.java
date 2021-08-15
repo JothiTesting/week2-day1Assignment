37144efd8227c3af29418008ecf55745f53137bb
Jothipriya <68255662+JothiTesting@users.noreply.github.com>
2021-08-14 22:20:57 -0500

    Add files via upload

---
+++ login.java
@@ -0,0 +1,26 @@
+package week2.day1;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 WebDriverManager.chromedriver().setup(); 
+		 ChromeDriver driver=new ChromeDriver(); 
+		 driver.get("http://leaftaps.com/opentaps/control/main");
+		 
+	// driver.close();
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		 driver.findElement(By.id("password")).sendKeys("crmsfa");
+		 driver.findElement(By.className("decorativeSubmit")).click();
+		 driver.findElement(By.linkText("CRM/SFA")).click();	 
+		 
+	}
+
+}
